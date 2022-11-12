5e2777053216af98afbf28531b3d273a60a365a5
Shell Raiser <ksshailesh05@gmail.com>
2022-11-12 20:02:25 +0530

    create dummy login form

--- pages/login.vue
+++ pages/login.vue
@@ -5,47 +5,66 @@
             |
             <a href="/login">Login</a>
         </nav>
-        <h1>Hi</h1>
+        <header>
+            <h1>Login / Sign-up</h1>
+            <!-- <div style="text-align: center;align-items:center; right: 50%;"> -->
+                <form action="">
+                    <label for="input-type-text">Username</label>
+                    <input id="input-type-text" type="text">
 
-        <button>Login</button>
+
+                    <label for="input-type-password">Input Password</label>
+                    <input id="input-type-password" type="password">
+                </form>
+            <!-- </div> -->
+            <button>Login</button>
+            or
+            <button>Sign-up</button>
+        </header>
+
+        <!-- <img :src=myFile /> -->
+        <!-- <div id="container"></div> -->
         <!-- {publicKey && <div>user: {publicKey}</div>} -->
     </div>
 </template>
 <script>
 import fleekStorage from '@fleekhq/fleek-storage-js';
-import { async } from 'q';
-// import 'fs'
-// require('dotenv').config()
+var myFile;
 
 export default {
     name: 'Login',
-    // data: {
-    // apiKey: process.env.FLEEK_API_KEY,
-    // apiSecret: process.env.FLEEK_API_SECRET
-    // },
     mounted() {
         // const apiKey = process.env.FLEEK_API_KEY;
         // console.log(apiKey)
         // const apiSecret = process.env.FLEEK_API_SECRET;
-        var myFile;
         async function caller() {
-            myFile = await fleekStorage.get({
-                apiKey: process.env.FLEEK_API_KEY,
-                apiSecret: process.env.FLEEK_API_SECRET,
-                key: 'bafybeib6c7os6b5tpl6zgcm7c3xyxld36ogvfoloz77edycf23pzekf4ju',
-                getOptions: [
-                    'data',
-                    'bucket',
-                    'key',
-                    'hash',
-                    'publicUrl'
-                ],
-            })
-            return myFile
+            // myFile = await fleekStorage.get({
+            //     apiKey: process.env.FLEEK_API_KEY,
+            //     apiSecret: process.env.FLEEK_API_SECRET,
+            //     key: '1659972572813.png',
+            //     getOptions: [
+            //         'data',
+            //         'bucket',
+            //         'key',
+            //         'hash',
+            //         'publicUrl'
+            //     ],
+            // })
+            // myFile = await fleekStorage.getFileFromHash({
+            //     hash: 'bafybeib6c7os6b5tpl6zgcm7c3xyxld36ogvfoloz77edycf23pzekf4ju',
+            // })
+            // console.log(myFile)
+            // return myFile
+
         }
-        console.log(caller())
+
+        // console.log(caller())
+        // caller()
 
     },
+    // data: {
+    //     theImg: myFile
+    // },
     // data:{
     // },
     methods: {
