20828eb3c9ec826c8c44c4528ab969ea7d10783a
Shell Raiser <ksshailesh05@gmail.com>
2022-11-12 22:29:10 +0530

    create sign component

---
+++ components/sign.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <form v-if="!$store.state.loginData.user">
+            <label for="username">Username</label>
+            <input id="username" type="text">
+
+            <label for="password">Input Password</label>
+            <input id="password" type="password">
+        </form>
+        <!-- </div> -->
+        <div v-if="!$store.state.loginData.user">
+            <button @click="login">Login</button>
+            or
+            <button>Sign-up</button>
+        </div>
+
+    </header>
+</template>
+<script>
+export default {
+    methods: {
+        login() {
+            this.$store.commit('loginData/toggle')
+        },
+        
+    }
+}
+</script>
