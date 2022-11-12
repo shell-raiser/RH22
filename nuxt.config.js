2d381b6a3d525c8d47b50b4e8b0f6369b571a685
Shell Raiser <ksshailesh05@gmail.com>
2022-11-12 11:33:20 +0530

    nuxt init and port native html to index.vue

---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+// import 'efy/efy.css';
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    htmlAttrs: {
+      lang: 'en',
+      efy_mode: "dark_nord",
+      style: "--efy_color2_var: 116,143,252;"
+    },
+    title: 'rh22',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: "efy/efy.css" },
+      { rel: 'stylesheet', href: "efy/demo/efy_config.css" },
+
+    ],
+    script: [
+      { src: "./efy/efy.js" }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
